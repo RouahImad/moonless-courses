b7fc6c76cca124e1f312e12c0e331e8179d04af1
Imadrouah <irouah2@gmail.com>
2024-01-14 01:55:18 +0100

    updates

--- maj.js
+++ maj.js
@@ -1,184 +1,160 @@
-let search = document.querySelector(".intro .search .box input");
-let options = document.querySelectorAll(".intro .search .options input");
-let searchIcon = document.querySelector(".intro .search .box i");
-let sideLi = document.querySelectorAll(".sidebar ul li a");
-let go = document.querySelector(".content img");
-let posin = document.querySelector(".page .posIn");
-let siLi = document.querySelectorAll(".content .wrapper > div");
-let mug = document.querySelector("#about .mug");
-let mon = document.querySelector(".mon");
-let sm = document.querySelectorAll(".gi span");
-let reg = new RegExp(`^[-=!@#$%^&*().,/0-9]+$`);
-let notification = document.querySelector(".notification"),
-    not = document.querySelector(".notification-div"),
-    notiHolder = document.querySelector(".notification-content"),
-    closeNoti = document.querySelector(".close");
-let darklight = document.querySelector(".header .theme");
+const elements = {
+    search: document.querySelector(".intro .search .box input"),
+    options: document.querySelectorAll(".intro .search .options input"),
+    searchIcon: document.querySelector(".intro .search .box i"),
+    sideLi: document.querySelectorAll(".sidebar ul li a"),
+    go: document.querySelector(".content img"),
+    posin: document.querySelector(".page .posIn"),
+    siLi: document.querySelectorAll(".content .wrapper > div"),
+    mug: document.querySelector("#about .mug"),
+    mon: document.querySelector(".mon"),
+    sm: document.querySelectorAll(".gi span"),
+    reg: /^[-=!@#$%^&*().,/0-9]+$/,
+    notification: document.querySelector(".notification"),
+    not: document.querySelector(".notification-div"),
+    notiHolder: document.querySelector(".notification-content"),
+    closeNoti: document.querySelector(".close"),
+    darklight: document.querySelector(".header .theme"),
+};
 
-spanNoti = document.createElement("span");
+const spanNoti = document.createElement("span");
 spanNoti.innerText = "You Have No New Notification";
-notiHolder.append(spanNoti);
-sideLi.forEach((li) => {
+elements.notiHolder.append(spanNoti);
+
+elements.sideLi.forEach((li) => {
     li.addEventListener("click", () => {
-        sideLi.forEach((l) => {
-            l.classList.remove("active");
-        });
+        elements.sideLi.forEach((l) => l.classList.remove("active"));
         li.classList.add("active");
     });
-});
-sideLi.forEach((l) => {
-    if (!sideLi[1].classList.contains("active")) {
-        l.addEventListener("mouseenter", function () {
-            l.classList.add("active");
-        });
 
-        l.addEventListener("mouseleave", function () {
-            l.classList.remove("active");
-        });
+    if (!elements.sideLi[1].classList.contains("active")) {
+        li.addEventListener("mouseenter", () => li.classList.add("active"));
+        li.addEventListener("mouseleave", () => li.classList.remove("active"));
     }
 });
-mon.addEventListener("click", () => {
-    sideLi.forEach((li) => {
-        li.classList.remove("active");
-    });
+
+elements.mon.addEventListener("click", () => {
+    elements.sideLi.forEach((li) => li.classList.remove("active"));
 });
 
-go.addEventListener("click", (e) => {
+elements.go.addEventListener("click", () => {
     window.scrollTo(0, 0);
-    go.style.setProperty("bottom", "65vh");
-    sideLi.forEach((li) => {
-        li.classList.remove("active");
-    });
-    setTimeout(function () {
-        go.style.setProperty("bottom", "40px");
-    }, 1000);
+    elements.go.style.setProperty("bottom", "65vh");
+    elements.sideLi.forEach((li) => li.classList.remove("active"));
+    setTimeout(() => elements.go.style.setProperty("bottom", "40px"), 1000);
 });
 
 window.addEventListener("scroll", () => {
-    sideLi.forEach((l) => {
-        l.is;
-        l.classList.remove("active");
-    });
-    let { scrollHeight, scrollTop, clientHeight } = document.documentElement;
+    elements.sideLi.forEach((l) => l.classList.remove("active"));
+    const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
     const scrolled = (scrollTop / (scrollHeight - clientHeight)) * 100;
-    posin.style.width = `${scrolled}%`;
+    elements.posin.style.width = `${scrolled}%`;
+
     if (window.scrollY < 350) {
-        go.style.opacity = 0;
-        go.classList.remove("clicked");
+        elements.go.style.opacity = 0;
+        elements.go.classList.remove("clicked");
     } else {
-        go.style.opacity = 1;
+        elements.go.style.opacity = 1;
     }
+
     let index;
-    if (scrollTop >= siLi[0].offsetTop - 80) {
-        siLi.forEach((e, i) => {
-            sideLi.forEach((li) => {
-                li.classList.remove("active");
-            });
-            if (e.offsetTop - 100 <= document.documentElement.scrollTop) {
+    if (scrollTop >= elements.siLi[0].offsetTop - 80) {
+        elements.siLi.forEach((e, i) => {
+            if (e.offsetTop - 100 <= scrollTop) {
                 index = i;
             }
         });
-        sideLi[index].classList.add("active");
-    } else {
-        sideLi.forEach((li) => {
-            li.classList.remove("active");
-        });
+        elements.sideLi[index].classList.add("active");
+    } else if (scrollHeight <= (scrollTop + clientHeight).toFixed()) {
+        index = elements.sideLi.length - 1;
+        elements.sideLi[index - 1].classList.add("active");
     }
-    if (scrollHeight <= (scrollTop + clientHeight).toFixed()) {
-        sideLi.forEach((li, i) => {
-            index = i;
-            li.classList.remove("active");
-        });
-        sideLi[index - 1].classList.add("active");
-    }
-    if (sideLi[sideLi.length - 2].classList.contains("active")) {
-        mug.classList.add("mugetsu");
-    } else {
-        mug.classList.remove("mugetsu");
-    }
+
+    elements.mug.classList.toggle(
+        "mugetsu",
+        elements.sideLi[elements.sideLi.length - 2].classList.contains("active")
+    );
 });
 
-sm.forEach((s) => {
+elements.sm.forEach((s) => {
     s.addEventListener("click", () => {
-        let s1L = document.querySelectorAll(
+        const s1L = document.querySelectorAll(
             `#${s.parentElement.parentElement.parentElement.id} .gi .${s.textContent} li`
         );
-        let i = s.firstElementChild;
+        const i = s.firstElementChild;
         i.classList.toggle("fa-arrow-up-wide-short");
         i.classList.toggle("fa-arrow-down-short-wide");
-        s1L.forEach((li) => {
-            li.classList.toggle("ac");
-        });
+        s1L.forEach((li) => li.classList.toggle("ac"));
     });
 });
-
 const chargeLevel = document.getElementById("charge-level");
 const charge = document.getElementById("charge");
+
 window.onload = () => {
     if (!navigator.getBattery) {
-        console.log("battery status api is not supported");
+        console.log("Battery status API is not supported");
         return false;
     }
+
+    navigator.getBattery().then((battery) => {
+        function update() {
+            updateCharging();
+            updateLeveling();
+        }
+
+        update();
+
+        battery.addEventListener("chargingchange", update);
+        battery.addEventListener("levelchange", update);
+
+        function updateCharging() {
+            charge.classList.toggle("active-charging", battery.charging);
+        }
+
+        function updateLeveling() {
+            const batteryLevel = `${parseInt(battery.level * 100)}%`;
+            charge.style.width = batteryLevel;
+            chargeLevel.textContent = batteryLevel;
+
+            if (parseInt(battery.level * 100) >= 70) {
+                document.documentElement.style.setProperty(
+                    "--charge-color",
+                    "#00FF00"
+                );
+            } else if (parseInt(battery.level * 100) >= 30) {
+                document.documentElement.style.setProperty(
+                    "--charge-color",
+                    "#FFA500"
+                );
+            } else {
+                document.documentElement.style.setProperty(
+                    "--charge-color",
+                    "#FF0000"
+                );
+            }
+        }
+    });
 };
 
-navigator.getBattery().then((battery) => {
-    function update() {
-        updateCharging();
-        updateLeveling();
-    }
-    update();
-    battery.addEventListener("chargingchange", () => {
-        update();
-    });
-    battery.addEventListener("levelchange", () => {
-        update();
-    });
-
-    function updateCharging() {
-        if (battery.charging) {
-            charge.classList.add("active-charging");
-        } else {
-            charge.classList.remove("active-charging");
-        }
-    }
-    function updateLeveling() {
-        let batteryLevel = `${parseInt(battery.level * 100)}%`;
-        charge.style.width = batteryLevel;
-        chargeLevel.textContent = batteryLevel;
-        if (parseInt(battery.level * 100) >= 70) {
-            document.documentElement.style.setProperty(
-                "--charge-color",
-                "#00FF00"
-            );
-        } else if (parseInt(battery.level * 100) >= 30) {
-            document.documentElement.style.setProperty(
-                "--charge-color",
-                "#FFA500"
-            );
-        } else {
-            document.documentElement.style.setProperty(
-                "--charge-color",
-                "#FF0000"
-            );
-        }
-    }
-});
-
-search.onfocus = () => {
-    search.style.width = "17rem";
+elements.search.onfocus = () => {
+    elements.search.style.width = "17rem";
 };
-search.addEventListener("keydown", (e) => {
-    if (e.key == "Enter" && search.value.length >= 3) {
+
+elements.search.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && elements.search.value.length >= 3) {
         handleSearch();
     }
-    if (reg.test(e.key)) {
+    if (elements.reg.test(e.key)) {
         e.preventDefault();
     }
 });
+
+elements.searchIcon.onclick = handleSearch;
+
 function handleSearch() {
-    if (search.value !== "" && search.value.length >= 3) {
+    if (elements.search.value !== "" && elements.search.value.length >= 3) {
         validate(
-            search.value
+            elements.search.value
                 .normalize("NFD")
                 .replace(/[\u0300-\u036f]/g, "")
                 .toLowerCase()
@@ -186,90 +162,105 @@ function handleSearch() {
     }
 }
 
-searchIcon.onclick = handleSearch;
-
 function validate(v) {
-    let valar = new Array();
+    const valar = [];
     document.querySelectorAll(`body *`).forEach((e) => {
         e.classList.remove("found");
     });
+
     let searchId;
-    options.forEach((op) => {
+    elements.options.forEach((op) => {
         if (op.checked) {
             searchId = op.id;
         }
     });
-    let ulNoti = document.createElement("ul");
+
+    const ulNoti = document.createElement("ul");
+
     document.querySelectorAll(`body #${searchId} *`).forEach((e) => {
         if (e.hasAttribute("data-search")) {
             if (
-                e.innerText.split("\n").splice(0, 1).join("").toLowerCase() == v
+                e.innerText.split("\n").splice(0, 1).join("").toLowerCase() ===
+                v
             ) {
                 if (!e.classList.contains("ac")) {
                     e.parentElement.firstElementChild.click();
                 }
-                notiHolder.childNodes.forEach((el) => {
+
+                elements.notiHolder.childNodes.forEach((el) => {
                     el.remove();
                 });
+
                 e.scrollIntoView({ behavior: "smooth", block: "start" });
+
                 spanNoti = document.createElement("span");
                 spanNoti.innerText = "You Have No New Notification";
-                notiHolder.append(spanNoti);
+                elements.notiHolder.append(spanNoti);
+
                 e.classList.add("found");
-                console.log("found");
-            } else if (e.getAttribute("data-search").split(" ").includes(v)) {
+            } else if (e.getAttribute("data-search").includes(v)) {
                 valar.push(e);
-                notiHolder.childNodes.forEach((el) => {
+
+                elements.notiHolder.childNodes.forEach((el) => {
                     el.remove();
                 });
-                not.classList.add("active");
-                let liNoti = document.createElement("li");
+
+                elements.not.classList.add("active");
+                const liNoti = document.createElement("li");
                 liNoti.textContent = e.innerText.split("\n").splice(0, 1);
                 ulNoti.append(liNoti);
-                notiHolder.append(ulNoti);
+                elements.notiHolder.append(ulNoti);
+
                 notiSliding(valar);
-                console.log(v);
-                console.log(e.getAttribute("data-search").toLowerCase());
-                console.log(e.getAttribute("data-search").toLowerCase() != v);
             }
         }
     });
 }
+
 function notiSliding(arr) {
-    let ntLi = document.querySelectorAll(".notification-div ul li");
+    const ntLi = document.querySelectorAll(".notification-div ul li");
+
     ntLi.forEach((el, i) => {
         el.addEventListener("click", () => {
             document.querySelectorAll(`body *`).forEach((e) => {
                 e.classList.remove("found");
             });
+
             arr[i].classList.add("found");
-            closeNoti.click();
+            elements.closeNoti.click();
+
             if (!arr[i].classList.contains("ac")) {
                 arr[i].parentElement.firstElementChild.click();
             }
+
             arr[i].scrollIntoView({ behavior: "smooth", block: "start" });
         });
     });
 }
 
-notification.addEventListener("click", (e) => {
+elements.notification.addEventListener("click", () => {
     click = true;
-    not.classList.add("active");
+    elements.not.classList.add("active");
+
     if (click) {
-        notification.classList.add("clicked");
+        elements.notification.classList.add("clicked");
     } else {
-        notification.classList.remove("clicked");
+        elements.notification.classList.remove("clicked");
     }
 });
-closeNoti.addEventListener("click", () => {
-    not.classList.remove("active");
+
+elements.closeNoti.addEventListener("click", () => {
+    elements.not.classList.remove("active");
 });
-darklight.addEventListener("click", () => {
+
+elements.darklight.addEventListener("click", () => {
     document.body.classList.toggle("dark");
+
     if (document.body.classList.contains("dark")) {
-        darklight.lastElementChild.textContent = "dark";
+        elements.darklight.lastElementChild.textContent = "dark";
     } else {
-        darklight.lastElementChild.textContent = "light";
+        elements.darklight.lastElementChild.textContent = "light";
     }
 });
-// document.querySelectorAll(".mod")[14].getAttribute("data-search").split(" ").slice(0,2)
+
+// End of your code...
